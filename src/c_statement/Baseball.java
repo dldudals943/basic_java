603fac05c4e14f917bc25597280619ef6f1e8e1f
DESKTOP-QH0HMQC\PC-13 <lass9436@gmail.com>
2021-03-16 17:24:47 +0900

    8교시 끝

---
+++ src/c_statement/Baseball.java
@@ -0,0 +1,120 @@
+package c_statement;
+
+import java.util.Scanner;
+
+public class Baseball {
+
+	public static void main(String[] args) {
+		/*
+		 * 123 1~9사이에 중복되지 않는 3개의 숫자
+		 * 135 라고 불렀다고 한다면 숫자와 정답을 비교합니다.
+		 * 1strike(1과 수와 자릿수가 같음) 1ball(3이 있긴 하지만 자릿수가 다름) 1out(5는 존재하지 않음)
+		 * 
+		 * 
+		 * 
+		 * 768 : 1S 0B 2O
+		 * 743 : 0S 1B 2O
+		 * 543 : 0S 1B 2O
+		 * 924 : 1S 1B 1O
+		 * 461 : 1S 0B 2O
+		 * 963 : 0S 0B 3O
+		 * 524 : 1S 1B 1O
+		 * 254 : 0S 2B 1O
+		 * 219 : 0S 1B 2O
+		 * 164 : 0S 1B 2O
+		 * 428 : 3S
+		 */
+
+		int a = (int)(Math.random()*9)+1;
+		int b = (int)(Math.random()*9)+1;
+		int c = (int)(Math.random()*9)+1;
+		
+		while( a == b || b == c || c == a ){
+			if(a == b){
+				b = (int)(Math.random()*9)+1;
+			}
+			if(b == c){
+				c = (int)(Math.random()*9)+1;
+			}
+			if(c == a){
+				a = (int)(Math.random()*9)+1;
+			}
+		}
+		
+		Scanner s = new Scanner(System.in);
+		int x=0;
+		int y=0;
+		int z=0;
+		int input = 0;
+		
+		while(a!=x || b!=y || c!=z){
+		System.out.println("각 자리수가 중복하지 않는 3자리의 숫자를 입력해주세요>");
+
+		
+		input = Integer.parseInt(s.nextLine());
+		x = (input%1000)/100;
+		y = (input%100)/10;
+		z = (input%10);
+		
+		int strike = 0;
+		int ball = 0;
+		int out = 0;
+		
+		//스트라이크 체크
+			if(a==x){
+				strike +=1;
+			}
+			if(b==y){
+				strike +=1;
+			}
+			if(c==z){
+				strike +=1;
+			}
+		//볼 체크
+			if(a==y || a==z){
+				ball +=1;
+			}
+			if(b==x || b==z){
+				ball +=1;
+			}
+			if(c==x || c==y){
+				ball +=1;
+			}
+		//아웃 체크
+		out = 3 - strike - ball;
+
+		System.out.println(strike + "S " + ball + "B " + out + "O");
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- src/c_statement/RepetitiveStatement.java
+++ src/c_statement/RepetitiveStatement.java
@@ -220,7 +220,14 @@ public class RepetitiveStatement {
 			}System.out.println();
 		}
 		
-		
+		for (int i = 1; i <= 5; i++){
+			for (int j = 1; j <= 5 - i; j++){
+					System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++){
+				System.out.print("*");
+			}System.out.println();
+		}		
 		
 		
 		
